4130369d9d011e470b03f9623c839004e125cff5
dragos-vlasie <dragos.vlasie@thisplace.com>
2019-04-24 09:58:52 +0100

    Setting up the project files

---
+++ src/components/Qustion.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Qustion extends Component {
+  render() {
+    return (
+      <div className='question'>
+        
+      </div>
+    )
+  }
+}
