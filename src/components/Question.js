1a8bfecdbab7eb74d1a72f5d334cc20a00d79d02
dragos-vlasie <dragos.vlasie@thisplace.com>
2019-08-02 16:04:27 +0100

    added new styling and a new structure to questions.js still need to fix some issues

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,10 +1,10 @@
-import axios from 'axios';
-import React, { Component } from 'react'
+import axios from "axios";
+import React, { Component } from "react";
 class Question extends Component {
   constructor(props) {
     super(props);
-    this.state = { 
-      events : [],
+    this.state = {
+      events: [],
       isHidden: false,
       questionNumber: 0,
       playerScore: 0
@@ -12,129 +12,161 @@ class Question extends Component {
   }
 
   componentDidMount() {
-    axios.get('https://opentdb.com/api.php?amount=10&category=11&difficulty=medium&type=multiple')
-    .then((response) => {
-      console.log(response);
-      this.setState({events: response.data.results})
-    })
-   .catch((error)=>{
-      console.log(error);
-   });
+    axios
+      .get(
+        "https://opentdb.com/api.php?amount=10&category=11&difficulty=easy&type=multiple"
+      )
+      .then(response => {
+        console.log(response);
+        this.setState({ events: response.data.results });
+      })
+      .catch(error => {
+        console.log(error);
+      });
   }
 
   encoding(unsafe) {
     return unsafe
-         .replace(/&amp;/g, "&")
-         .replace(/&lt;/g, "<")
-         .replace(/&gt;/g, ">")
-         .replace(/&quot;/g, "\"")
-         .replace(/&#039;/g, "'")
-         .replace(/&amp;#039;/g, "'")
-         .replace(/&ldquo;/g, "“")
-         .replace(/&rdquo;/g, "”")
-         .replace(/&aacute;/g, "á")
-         .replace(/&Eacute;/g, "É");
+      .replace(/&amp;/g, "&")
+      .replace(/&lt;/g, "<")
+      .replace(/&gt;/g, ">")
+      .replace(/&quot;/g, '"')
+      .replace(/&#039;/g, "'")
+      .replace(/&amp;#039;/g, "'")
+      .replace(/&ldquo;/g, "“")
+      .replace(/&rdquo;/g, "”")
+      .replace(/&aacute;/g, "á")
+      .replace(/&Eacute;/g, "É");
   }
-  
+
   showQuestion = () => {
     this.setState({
       isHidden: !this.state.isHidden
-    })
-  }
+    });
+  };
 
-  chosenAnswer= (event) => {
-    const questions  = this.state.events;
-    var questionCurrentNumber  = this.state.questionNumber;
+  chosenAnswer = event => {
+    const questions = this.state.events;
+    var questionCurrentNumber = this.state.questionNumber;
     let chosenAnswer = event.target.innerText;
     let correctAnswer = questions[questionCurrentNumber].correct_answer;
 
-    if(correctAnswer === chosenAnswer) {
-      console.log('SUCCESS:', "add to the score")
+    if (correctAnswer === chosenAnswer) {
+      console.log("SUCCESS:", "add to the score");
 
       this.setState({
         playerScore: this.incrementNumber(this.state.playerScore)
-      }) 
+      });
     } else {
-
-      console.log('de cact:', "subtract")
+      console.log("de cact:", "subtract");
 
       this.setState({
         playerScore: this.decrementNumber(this.state.playerScore)
-      }) 
+      });
     }
 
-    if(questionCurrentNumber === 9) {
-  
+    if (questionCurrentNumber === 9) {
+      // prepare for the api call
     }
     this.setState({
       questionNumber: this.incrementNumber(questionCurrentNumber)
-    }) 
-  }
+    });
+  };
 
-  incrementNumber = (increment) => {
+  incrementNumber = increment => {
     increment++;
+    // this.progress();
     return increment;
-  }
+  };
 
-  decrementNumber = (decrement) => {
+  decrementNumber = decrement => {
     decrement--;
     return decrement;
-  }
+  };
+
+  // progress() {
+  //   var elem = document.getElementById("myBar");
+  //   var width = (this.state.playerScore + 1) * 10;
+  //   console.log("width:", width);
+  //   if (width) {
+  //     elem.style.width = width + "%";
+  //   } else {
+  //     elem.style.width = "10%";
+  //   }
+  // }
 
   renderQuestion(number) {
-    const questions  = this.state.events;
-    let allAnswers
-    if(questions.length){
-      allAnswers = questions[number].incorrect_answers
-      allAnswers.push(questions[number].correct_answer)
+    const questions = this.state.events;
+    let allAnswers;
+    if (questions.length) {
+      allAnswers = questions[number].incorrect_answers;
+      allAnswers.push(questions[number].correct_answer);
       allAnswers.sort();
     }
 
     const questionList = questions.length ? (
       <div>
-        <h3>{this.encoding(questions[number].question)}</h3>
-
-        <div className="question-wrapper">{allAnswers.map(answers => <button onClick={(event) => this.chosenAnswer(event)}>{this.encoding(answers)}</button>)}</div>
         
+      <h3>{this.encoding(questions[number].question)}</h3>
+      <div id="answers" class="grid">
+
+          {allAnswers.map(answers => (
+            <button  class="answer" onClick={event => this.chosenAnswer(event)}>
+              {this.encoding(answers)}
+            </button>
+          ))}
       </div>
-    
+          </div>
     ) : (
       <div>no questions</div>
-    )
+    );
 
-    return questionList
+    return questionList;
   }
 
   render() {
-    
-    if(this.state.questionNumber !== 9) {
+    if (this.state.questionNumber !== 9) {
       return (
-        <div>
-          <p><span>Score</span> {this.state.playerScore}</p>
-          <div className='question'>
-            { !this.state.isHidden ? <button onClick={this.showQuestion}>Start</button> : null }
-            { this.state.isHidden ? this.renderQuestion(this.state.questionNumber) : null }
+        <div class="container">
+          <div class="feedback">
+            <h4 class="left">
+              Time Left: <span id="time">0</span>
+            </h4>
+            <h4 class="right">
+              Score: <span id="score">{this.state.playerScore}</span>
+            </h4>
           </div>
+            {!this.state.isHidden ? (
+              <button class="answer" onClick={this.showQuestion}>
+                Start
+              </button>
+            ) : null}
+            {this.state.isHidden
+              ? this.renderQuestion(this.state.questionNumber)
+              : null}
         </div>
-      )
+      );
     } else {
       return (
         <div>
-          <p><span>Score</span> {this.state.playerScore}</p>
-          <div>Choose the next category</div>
-          <div>
-            <button>Options1</button>
-            <button>Options1</button>
-            <button>Options3</button>
+          <div class="feedback">
+            <h4 class="left">
+              Time Left: <span id="time">0</span>
+            </h4>
+            <h4 class="right">
+              Score: <span id="score">{this.state.playerScore}</span>
+            </h4>
+          </div>
+          <h3 class="answer">Choose the next category</h3>
+          <div class="grid">
+            <button class="answer">Options1</button>
+            <button class="answer">Options1</button>
+            <button class="answer">Options3</button>
           </div>
         </div>
-      )
+      );
     }
   }
 }
 
 export default Question;
-
-
-
-
